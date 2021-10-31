bcf336055c0d79b3eb2166f59babb6c602e745cd
Shubhamdutta2000 <dondutta2000@gmail.com>
2021-10-31 19:31:29 +0530

    [NEW] added generateOTP API and eKYC API

---
+++ api/eKYC.js
@@ -0,0 +1,30 @@
+
+export const eKYC = async (txnNumber, otp, shareCode, uid) => {
+    try {
+        const response = await fetch("https://stage1.uidai.gov.in/eAadhaarService/api/downloadOfflineEkyc", {
+            method: 'POST',
+            mode: 'cors',
+            cache: 'no-cache',
+            credentials: 'same-origin',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            redirect: 'follow',
+            referrerPolicy: 'no-referrer',
+            body: JSON.stringify(
+                {
+                    "txnNumber": "mAadhaar:" + txnNumber,
+                    "otp": otp,
+                    "shareCode": shareCode,
+                    "uid": uid
+                }
+            )
+        })
+
+        const data = response.json();
+
+        return data
+    } catch (error) {
+        console.log(error)
+    }
+}
